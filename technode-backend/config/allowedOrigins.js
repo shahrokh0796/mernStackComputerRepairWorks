4a1f3ed9df16d0fb611e6f4e71bfc4e597d74d8d
shahrokh0796 <shahrokh07.96@gmail.com>
2024-10-11 23:11:44 +0530

    backend ready

--- technode-backend/config/allowedOrigins.js
+++ technode-backend/config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
-    'http://localhost:5173',
-    'http://localhost:3000',
-    'https://misterfasterrepairs.com'
-
+    // 'http://localhost:5173',
+    // 'http://localhost:3000',
+    // 'https://misterfasterrepairs.com'
+    'https://misterfasterrepairs.onrender.com'
 ];
 
 module.exports = allowedOrigins;
